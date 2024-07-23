4495b99bfba404e53b4bb4e2ab569eff0fb32f7f
Nelamin2 <nidalelamin@gmail.com>
2024-07-24 02:54:01 +0300

    tasks

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,48 +1,61 @@
 #!/usr/bin/python3
 """ LFU caching
 """
-BaseCaching = __import__('base_caching').BaseCaching
+from base_caching import BaseCaching
+from collections import OrderedDict
 
 
 class LFUCache(BaseCaching):
     """ LFU cache class """
-    class LFUCache(BaseCaching):
-        def __init__(self):
-            super().__init__()
-            self.frequency = {}
-            self.usage = {}
-            self.time = 0
+    def __init__(self):
+        """ Constructor """
+        super().__init__()
+        self.cache = OrderedDict()
+        self.freq = {}  # To keep track of frequencies
+        self.usage = {}  # To keep track of last access time
 
     def put(self, key, item):
+        """ Add an item in the cache """
         if key is None or item is None:
             return
-        if key in self.cache_data:
-            self.cache_data[key] = item
-            self.frequency[key] += 1
+        if key in self.cache:
+            # Update the value and frequency
+            self.cache[key] = item
+            self.freq[key] += 1
+            self.usage[key] = len(self.cache)
         else:
-            if len(self.cache_data) >= self.MAX_ITEMS:
-                # Find the LFU key to remove
-                lfu_keys = [k for k, v in self.frequency.items()
-                            if v == min(self.frequency.values())]
-                if len(lfu_keys) > 1:
-                    # If there's a tie, use the LRU key among the LFU keys
-                    lru_key = min(lfu_keys, key=lambda k: self.usage[k])
-                else:
-                    lru_key = lfu_keys[0]
-                print(f"DISCARD: {lru_key}")
-                del self.cache_data[lru_key]
-                del self.frequency[lru_key]
+            if len(self.cache) >= BaseCaching.MAX_ITEMS:
+                # Find the least frequently used item
+                min_freq = min(self.freq.values())
+                # Get all keys with the minimum frequency
+                min_freq_keys = [k for k,
+                                 v in self.freq.items() if v == min_freq]
+                # Use LRU among LFU keys
+                lru_key = min(min_freq_keys, key=lambda k: self.usage[k])
+                # Remove the LFU item
+                del self.cache[lru_key]
+                del self.freq[lru_key]
                 del self.usage[lru_key]
-            self.cache_data[key] = item
-            self.frequency[key] = 1
-        # Update usage time
-        self.time += 1
-        self.usage[key] = self.time
+                print(f"DISCARD: {lru_key}")
+            # Add the new item
+            self.cache[key] = item
+            self.freq[key] = 1
+            self.usage[key] = len(self.cache)
 
     def get(self, key):
-        if key is None or key not in self.cache_data:
+        """ Get an item by key """
+        if key is None or key not in self.cache:
             return None
-        self.frequency[key] += 1
-        self.time += 1
-        self.usage[key] = self.time
-        return self.cache_data[key]
+        # Update frequency and access time
+        self.freq[key] += 1
+        self.usage[key] = len(self.cache)
+        return self.cache[key]
+
+    def get(self, key):
+        """ Get an item by key """
+        if key in self.cache_data:
+            self.count[key] += 1
+            self.queue.remove(key)
+            self.queue.append(key)
+            return self.cache_data.get(key)
+        return None
