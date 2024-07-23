ae3f98201ad3acdd3622297bf6e390a3952e182b
Nelamin2 <nidalelamin@gmail.com>
2024-07-23 03:11:26 +0300

    tasks

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,9 +2,11 @@
 """
 a script that takes two integer arguments page and page_size
 and returns a tuple containing two integers
-start index and end index corresponding to the range of indexes 
+start index and end index corresponding to the range of indexes
 to return in a list for those particular pagination parameters
 """
+
+
 def index_range(page: int, page_size: int) -> tuple:
     """return a tuple of size two containing a start index and an end index"""
     return ((page - 1) * page_size, page * page_size)
